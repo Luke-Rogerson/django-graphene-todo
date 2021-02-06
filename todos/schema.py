fce454154a43a10d57442ca819b8d00d0a2af2c5
Luke Rogerson <contact@lukerogerson.me>
2021-02-06 16:34:47 +0000

    Add edit, delete and mark as done functionality

--- todos/schema.py
+++ todos/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
+from django.utils import timezone
 
 from .models import Todo
 
@@ -7,6 +8,7 @@ from .models import Todo
 class TodoType(DjangoObjectType):
     class Meta:
         model = Todo
+        exclude = ("deleted",)
 
 
 class Query(graphene.ObjectType):
@@ -20,7 +22,7 @@ class CreateTodo(graphene.Mutation):
     todo = graphene.Field(TodoType)
 
     class Arguments:
-        text = graphene.String()
+        text = graphene.String(required=True)
 
     def mutate(self, info, text: str):
         todo = Todo(text=text)
@@ -28,5 +30,51 @@ class CreateTodo(graphene.Mutation):
         return CreateTodo(todo=todo)
 
 
+class DeleteTodo(graphene.Mutation):
+    todo = graphene.Field(TodoType)
+
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    def mutate(self, info, id: str):
+        todo: Todo = Todo.objects.get(id=id)
+        todo.deleted = True
+        todo.save()
+        return DeleteTodo(todo=todo)
+
+
+class EditTodo(graphene.Mutation):
+    todo = graphene.Field(TodoType)
+
+    class Arguments:
+        id = graphene.ID(required=True)
+        text = graphene.String(required=True)
+
+    def mutate(self, info, id: str, text: str):
+        todo: Todo = Todo.objects.get(id=id)
+        todo.text = text
+        todo.date_updated = timezone.now()
+        todo.save()
+        return DeleteTodo(todo=todo)
+
+
+class MarkTodoAsDone(graphene.Mutation):
+    todo = graphene.Field(TodoType)
+
+    class Arguments:
+        id = graphene.ID(required=True)
+        status = graphene.Boolean()
+
+    def mutate(self, info, id: str, status=None):
+        todo: Todo = Todo.objects.get(id=id)
+        todo.completed = status if status is not None else not todo.completed
+        todo.date_updated = timezone.now()
+        todo.save()
+        return MarkTodoAsDone(todo=todo)
+
+
 class Mutation(graphene.ObjectType):
     create_todo = CreateTodo.Field()
+    delete_todo = DeleteTodo.Field()
+    edit_todo = EditTodo.Field()
+    mark_as_done = MarkTodoAsDone.Field()
