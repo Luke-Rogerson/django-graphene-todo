d52e59849e24de48d702210202c58e994ddc9e06
Luke Rogerson <contact@lukerogerson.me>
2021-02-07 21:53:33 +0000

    Pin PyJWT to fix django-graphql-jwt error

--- dgtodo/schema.py
+++ dgtodo/schema.py
@@ -12,7 +12,6 @@ class Mutation(users.schema.Mutation, todos.schema.Mutation, graphene.ObjectType
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
-    pass
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
