8e89ddc5eeddc8cbcf84b6d3a1b075078642c992
Luke Rogerson <contact@lukerogerson.me>
2021-02-10 22:22:29 +0000

    Update Todo model to use uuid pk

--- todos/models.py
+++ todos/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+import uuid
 
 
 class Todo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(default=None, null=True)
